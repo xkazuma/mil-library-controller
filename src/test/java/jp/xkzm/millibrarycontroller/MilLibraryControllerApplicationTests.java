2a584dad213d4e49f8499164038fe40d75c49f9e
xkazuma <kusu@mil.doshisha.ac.jp>
2022-07-05 12:04:09 +0900

    generated Spring Boot project with IntelliJ

---
+++ src/test/java/jp/xkzm/millibrarycontroller/MilLibraryControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.xkzm.millibrarycontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MilLibraryControllerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
